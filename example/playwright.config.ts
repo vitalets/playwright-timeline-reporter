ea57fd677848d5bca8c0443e572dc5381ff15ace
vitalets <noginsk@rambler.ru>
2026-04-03 20:28:12 +0200

    fix example

--- example/playwright.config.ts
+++ example/playwright.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  // fullyParallel: true,
   workers: 3,
   reporter: [
     // In a real project use: 'playwright-timeline-reporter' instead of '../dist/reporter'
     ['../dist/reporter'],
-    ['list'],
-    ['html', { open: 'never' }],
-    process.argv.includes('merge-reports') ? ['null'] : ['blob'],
+    ['blob'],
   ],
   use: {
     baseURL: 'https://playwright.dev/',
@@ -18,26 +15,9 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    // {
-    //   name: 'auth',
-    //   use: { ...devices['Desktop Chrome'] },
-    //   testMatch: /landing\.spec\.ts/,
-    // },
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
   ],
 });
