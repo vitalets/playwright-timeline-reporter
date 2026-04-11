661209a189321363f0e4069d4b0d414889e03c01
vitalets <noginsk@rambler.ru>
2026-04-11 16:24:33 +0200

    improve tests

--- test/e2e/parallel-mode-in-file/playwright.config.ts
+++ test/e2e/parallel-mode-in-file/playwright.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './specs',
-  workers: 2,
-  reporter: [['../../dist/index.js', { outputFile: './timeline-report/index.html' }]],
+  workers: 4,
+  reporter: [['../../../dist/index.js']],
 });
---
+++ test/e2e/parallel-mode-in-file/specs/spec1.spec.ts
@@ -0,0 +1,7 @@
+import { test } from '@playwright/test';
+import { setTimeout } from 'node:timers/promises';
+
+test('test 1', async () => {
+  await setTimeout(100);
+  throw new Error('Test error');
+});
